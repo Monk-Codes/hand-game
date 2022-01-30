23922b3e5499716f16b91d01f139fd73ccd80eb5
dizzy <84877191+drpratikk@users.noreply.github.com>
2022-01-30 11:36:26 +0530

    placing map

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import "./App.css";
+import { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    started: false,
+  };
+
+  render() {
+    const { started } = this.state;
+    const [Rock, Paper, Scissors] = [
+      "https://i.imgur.com/TONXH9s.png",
+      "https://i.imgur.com/t2154qR.png",
+      "https://i.imgur.com/SXstPKk.png"
+    ];
+    return (
+      <div className="App">
+        <h1>Rock Paper Scissors</h1>
+        {started ? (
+          <div className="Game">
+            <div className="Player">
+              <p>Player</p>
+              <div className="choose">
+                  {[
+                  [Rock, "Rock"],
+                  [Paper, "Paper"],
+                  [Scissors, "Scissors"]
+                ].map((a) => (
+                    <span key={a[1]}>
+                    <img src={a[0]} alt={a[1]} />
+                      {a[1]}
+                    </span>
+                  ))}
+              </div>
+            </div>
+            <div className="Computer">
+              <p>Computer</p>
+              <img src="https://i.imgur.com/CyvHqQH.png" alt="All Choices" />
+            </div>
+          </div>
+        ) : (
+          <img
+            src="https://i.imgur.com/iO8f1Lk.jpg"
+            alt="start"
+            className="start"
+            onClick={() => {
+              this.setState({ started: true });
+            }}
+          />
+        )}
+      </div>
+    );
+  }
 }
 
 export default App;
